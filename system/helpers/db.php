75229292149eb2ffc50312d30d36fb94852908ac
Håkan Nylén <hakan@dun.se>
2011-11-16 17:57:30 +0100

    security leak - fixed so when db settings is set, the global variable get unset. (this need to be checked so it work properly)

--- system/helpers/db.php
+++ system/helpers/db.php
@@ -9,6 +9,7 @@ class db {
 		$this->instance = &$this;
 		global $db_config;
 		$this->config = $db_config;
+		unset($db_config);
 		$this->conn = mysql_connect($this->config['db_host'], $this->config['db_user'], $this->config['db_password'])
     or die('Could not connect: ' . mysql_error());
     	mysql_select_db($this->config['db_dbname'], $this->conn) or die('Could not select database');
