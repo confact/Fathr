3f6caa977cd80d54953f8392e14252a5a8a7db18
Håkan Nylén <hakan@dun.se>
2011-11-16 01:15:37 +0100

    added model test in example.

--- application/controller/example.php
+++ application/controller/example.php
@@ -1,6 +1,11 @@
 <?php
 class Example extends Controller {
 
+	function __construct()
+	{
+		parent::__construct();
+	}
+
 	function index() {
 		echo "it Works!";
 		echo "<br /><a href='example/newpage'>how a other function can work</a>";
@@ -8,6 +13,12 @@ class Example extends Controller {
 	
 	public function newpage() {
 		echo "new page";
+	}
+	
+	public function testmodel() {
+		global $fathr;
+		$fathr->load->model("user");
+		$fathr->user->test();
 	}
 
 }
