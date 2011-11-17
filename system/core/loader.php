9ae4c9c9f396aa046efe4fea56c1ac573baadeef
Håkan Nylén <hakan@dun.se>
2011-11-17 01:35:28 +0100

    added support for up to 2 arguments. Need to be improved.

--- system/core/loader.php
+++ system/core/loader.php
@@ -11,6 +11,10 @@ class loader {
 		global $fathr;
 		$this->controllerpath = $fathr->config['applicationpath'] . '/controllers/';
 		$this->defaultcontroller = $fathr->config['default_controller'];
+		if(!isset($_GET['page']))
+		{
+			$_GET['page'] = "";
+		}
 		$this->url = $this->seperatenames($_GET['page']);
 	}
 
@@ -40,13 +44,35 @@ class loader {
 
 	private function alive()
 	{
-		if($this->url[1] != NULL)
+		if(isset($this->url[1]) AND $this->url[1] != NULL)
 		{
 			$function = $this->url[1];
-			
+			$parameters = count($this->url)-2;
 			if(method_exists($this->controller, $function))
 			{
-				$this->controller->$function();
+				$method = new ReflectionMethod(get_class($this->controller), $function);
+				$num = $method->getNumberOfParameters();
+				if($num > 0) {
+					
+					if($parameters == $num) {
+						if($parameters == 1) {
+							$this->controller->$function($this->url[2]);
+						}
+						else {
+							$this->controller->$function($this->url[2],$this->url[3]);
+						}
+					}
+					else
+					{
+						$this->show_error(406);
+					}
+				}
+				else if($num <= 0 AND $parameters > 0){
+					$this->show_error(402);
+				}
+				else {
+					$this->controller->$function();
+				}
 			}
 			else 
 			{
@@ -59,7 +85,13 @@ class loader {
 	}
 	private function seperatenames($url)
 	{
-		return explode('/',$url);
+		$slug = explode('/',$url);
+		foreach ($slug as $key => $value) {
+  			if (is_null($value)) {
+    			unset($my[$key]);
+  			}
+		} 
+		return $slug;
 	}
 	
 	private function show_error($errorid = "500")
