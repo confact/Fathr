18555715e4f53bf68d75c5440d03fa3ea86dbe12
Håkan Nylén <hakan@dun.se>
2012-01-06 15:30:18 +0100

    fixed better code in view.

--- application/views/fathr_admininlogged.php
+++ application/views/fathr_admininlogged.php
@@ -6,19 +6,19 @@
 			<div class="clearfix">
               <div class="input-prepend">
                 <span class="add-on">Site name</span>
-                <input class="medium" id="sitename" name="sitename" size="16" type="text" value="<?    	global $fathr; echo $fathr->controller->settings['sitename']; ?>" />
+                <input class="medium" id="sitename" name="sitename" size="16" type="text" value="<? echo $fathr->controller->settings['sitename']; ?>" />
               </div>
             </div>
             <div class="clearfix">
             <div class="input-prepend">
                 <span class="add-on">url</span>
-                <input class="medium" id="url" name="url" size="16" type="text" value="<?    	global $fathr; echo $fathr->controller->settings['url']; ?>" />
+                <input class="medium" id="url" name="url" size="16" type="text" value="<? echo $fathr->controller->settings['url']; ?>" />
             </div>
             </div>
              <div class="clearfix">
             <div class="input-prepend">
                 <span class="add-on">theme</span>
-                <input class="medium" id="theme" name="theme" size="16" type="text" value="<?    	global $fathr; echo $fathr->controller->settings['theme']; ?>" />
+                <input class="medium" id="theme" name="theme" size="16" type="text" value="<? echo $fathr->controller->settings['theme']; ?>" />
             </div>
             </div>
             <input class="btn success" type="submit" name="Update" value="Update" />
