e0c708a66b3a1d896e3db8af753542712272c6e2
Håkan Nylén <hakan@dun.se>
2013-05-14 15:55:53 +0200

    just started with PHPUnit, need to be fixed.
    committing this because i'm changing computer.

---
+++ phpunit/bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+// Errors on full!
+ini_set('display_errors', 1);
+error_reporting(E_ALL | E_STRICT);
+
+
+require_once('config.php');
+
+require_once('system/boot.php');
+
+?>
---
+++ phpunit/config.php
@@ -0,0 +1,22 @@
+<?php
+
+/* This is the config-file. */
+
+//development mode
+$config['developmentmode'] = true;
+
+// the path to the site. Example: www.example.com/fathr/ - then sitepath is fathr/
+$config['sitepath'] = "";
+
+// the "tag"/prefix for the tables in  fathr cms. "tag_"{settings} as example.
+$config['table_tag'] = "fathr_";
+
+// Applicationpath
+$config['applicationpath'] = "./application";
+
+// This is the default controller you will use if no page is set
+$config['default_controller'] = "fathr_frontcontroller";
+
+// use front controller/theme engine directly from the core?
+$config['theme_from_core'] = true;
+?>
