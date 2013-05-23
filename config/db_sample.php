857f79815055bedebdd496b3e2bd0ac647f6a6ae
Håkan Nylén <hakan@dun.se>
2013-05-23 18:45:58 +0200

    corrected config to sample again.

---
+++ config/db_sample.php
@@ -0,0 +1,12 @@
+<?php
+// This is the config for the database helper
+
+$db_config['db_host'] = "";
+$db_config['db_dbname'] = "";
+$db_config['db_user'] = "";
+$db_config['db_password'] = "";
+
+// use mysqli or not
+$db_config['mysqli'] = false;
+
+?>
