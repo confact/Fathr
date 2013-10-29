cef312816f770cf199ade2541b225ae1a664b9ee
Håkan Nylén <hakan@dun.se>
2013-10-29 11:02:05 +0100

    structured model class correct and added php doc.

--- system/core/model.php
+++ system/core/model.php
@@ -1,24 +1,33 @@
 <?php
+
+/**
+ * the core-model which your models MUST extend. 
+ * Adding support for database automatically and load of course.
+ */
 class Model {
-// the core-model which your models MUST extend. Adding support for database automatically and load of course.
-	public $load;
-	
-	function __construct($db = false)
-	{
-		global $fathr;
-		if($db)
-		{
-			if(!isset($fathr->db)) {
-				if(!class_exists("db"))
-				{
-					require_once('system/helpers/db.php');
-				}
-				$this->fathr = new db();
-			}
-			$this->db = &$fathr->db;
-		}
-		$this->config = &$fathr->config;
-		$this->load = new load($this);
-	}
+
+    public $load;
+
+    /**
+     * 
+     * @global Father $fathr
+     * @param boolean $db
+     */
+    function __construct($db = false) {
+        global $fathr;
+        if ($db) {
+            if (!isset($fathr->db)) {
+                if (!class_exists("db")) {
+                    require_once('system/helpers/db.php');
+                }
+                $this->fathr = new db();
+            }
+            $this->db = &$fathr->db;
+        }
+        $this->config = &$fathr->config;
+        $this->load = new load($this);
+    }
+
 }
+
 ?>
