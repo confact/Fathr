7b2ad9f69fb9cc7b6a3921638491926e8117afb1
Håkan Nylén <hakan@dun.se>
2013-10-29 10:56:14 +0100

    Structured and added correct php documentation for the functions

--- system/core/controller.php
+++ system/core/controller.php
@@ -1,18 +1,27 @@
 <?php
+
+/**
+ * A simple core-controller your controller MUST extend to get the configs and the load features.
+ */
 class Controller {
-	//A simple core-controller your controller MUST extend to get the configs and the load features.
-	public $load;
-	
-	//install the right stuff for every controller. making it easy to use and load new helpers and views.
-	function __construct()
-	{
-		global $fathr;
-		$this->load = new load($this);
-		$this->config = &$fathr->config;
-		if(isset($fathr->theme))
-		{
-			$this->theme = &$fathr->theme;
-		}
-	}
+
+    public $load;
+
+    /**
+     * install the right stuff for every controller. 
+     * making it easy to use and load new helpers and views.
+     * 
+     * @global type $fathr
+     */
+    function __construct() {
+        global $fathr;
+        $this->load = new load($this);
+        $this->config = &$fathr->config;
+        if (isset($fathr->theme)) {
+            $this->theme = &$fathr->theme;
+        }
+    }
+
 }
+
 ?>
--- system/core/load.php
+++ system/core/load.php
@@ -1,59 +1,89 @@
 <?php
-class load
-{
-// The class that helps the user to load helpers, models and views very easy, by calling this functions bellow.
-	function __construct(&$controller) {
-		$this->contr = &$controller;
-	}
-	
-	//just simple include the view.
-	function view($name)
-	{
-		global $fathr;
-		if(file_exists($fathr->config['applicationpath'] . '/views/' . $name . '.php')) {
-			include($fathr->config['applicationpath'] . '/views/' . $name . '.php');
-		}
-	}
-	
-	//check if the model should load db, then save it to the controller.
-	function model($name, $db = false)
-	{
-		global $fathr;
-		if(file_exists($fathr->config['applicationpath'] . '/models/' . $name . '.php')) {
-			$modelname = ucfirst($name);
-			if(!isset($this->contr->$name)) {
-				$this->contr->$name = new $modelname($db);
-			}
-			else {
-				throw new Exception("Model already set.");
-			}
-		}
-		else {
-			throw new Exception("Model at path ".$fathr->config['applicationpath'] . '/models/' . $name . '.php'."doesn't exist.");
-		}
-	}
-		
-	//check if the helper is the db or not, then save it to the controller.
-	function helper($name)
-	{
-		global $fathr;
-		if(file_exists($fathr->config['systempath'] . '/helpers/' . $name . '.php')) {
-			if($name == "db")
-			{
-				$modelname = ucfirst($name);
-				$fathr->db = new $modelname();
-				$this->contr->$name = &$fathr->db;
-			}
-			else {
-				$modelname = ucfirst($name);
-				if(!isset($this->contr->$name)) {
-					$this->contr->$name = new $modelname();
-				}
-			}
-		}
-		else {
-			throw new Exception("helper doesn't exist.");
-		}
-	}
+
+/**
+ * The class that helps the user to load helpers, 
+ * models and views very easy, by calling this functions bellow.
+ * @author confact <hakan@dun.se>
+ */
+class load {
+
+    /**
+     * 
+     * @param Controller $controller
+     */
+    function __construct(&$controller) {
+        $this->contr = &$controller;
+    }
+
+    /**
+     * just simple include the view.
+     * 
+     * @global Father $fathr
+     * @param string $name
+     * @param boolean $return
+     * 
+     * @return void|string
+     */
+    function view($name, $return = false) {
+        global $fathr;
+        if (file_exists($fathr->config['applicationpath'] . '/views/' . $name . '.php')) {
+            if ($return) {
+                $returner = include($fathr->config['applicationpath'] . '/views/' . $name . '.php');
+                return $returner;
+            } else {
+                include($fathr->config['applicationpath'] . '/views/' . $name . '.php');
+            }
+        }
+    }
+
+    /**
+     * check if the model should load db, then save it to the controller.
+     * 
+     * @global Father $fathr
+     * @param string $name
+     * @param boolean $db
+     * 
+     * @throws Exception
+     */
+    function model($name, $db = false) {
+        global $fathr;
+        if (file_exists($fathr->config['applicationpath'] . '/models/' . $name . '.php')) {
+            $modelname = ucfirst($name);
+            if (!isset($this->contr->$name)) {
+                $this->contr->$name = new $modelname($db);
+            } else {
+                throw new Exception("Model already set.");
+            }
+        } else {
+            throw new Exception("Model at path " . $fathr->config['applicationpath'] . '/models/' . $name . '.php' . "doesn't exist.");
+        }
+    }
+
+    /**
+     * check if the helper is the db or not, then save it to the controller.
+     * 
+     * @global Father $fathr
+     * @param string $name
+     * @throws Exception
+     */
+    function helper($name) {
+        global $fathr;
+        if (file_exists($fathr->config['systempath'] . '/helpers/' . $name . '.php')) {
+            if ($name == "db") {
+                $modelname = ucfirst($name);
+                $fathr->db = new $modelname();
+                $this->contr->$name = &$fathr->db;
+            } else {
+                $modelname = ucfirst($name);
+                if (!isset($this->contr->$name)) {
+                    $this->contr->$name = new $modelname();
+                }
+            }
+        } else {
+            throw new Exception("helper doesn't exist.");
+        }
+    }
+
 }
+
 ?>
