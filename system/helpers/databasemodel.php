edab71ea31a9bc21ae1983cc5f3cd23e2b0c43a4
Håkan Nylén <hakan@dun.se>
2013-10-31 09:38:11 +0100

    added phpdoc for the rest of the files.

--- system/helpers/databasemodel.php
+++ system/helpers/databasemodel.php
@@ -1,52 +1,73 @@
 <?php
+/**
+ * The class handling the data from db-queries.
+ */
 class Databasemodel {
-	private $array = array();
-	
-	function __construct($result)
-	{
-		if(!is_bool($result)) {
-		if(!is_resource($result) && get_class($result) == "mysqli_result")
-		{
-			while($row=$result->fetch_array(MYSQLI_ASSOC))
-			{
-				foreach ($row as $key => $val) {
-					if (is_int($key)) {
-						unset($row[$key]);
-					}
-				}
-				$this->array[] = $row;
-			}
-		}
-		else {
-			while($row=mysql_fetch_array($result))
-			{
-				foreach ($row as $key => $val) {
-					if (is_int($key)) {
-						unset($row[$key]);
-					}
-				}
-				$this->array[] = $row;
-			}
-		}
-		}
-		else {
-			return false;
-		}
-	}
-	
-	public function getArray()
-	{
-		return $this->array;
-	}
-	
-	function num_rows()
-	{
-		return count($this->array);
-	}
-	
-	function num_keys()
-	{
-		return count(array_keys($this->array));
-	}
+
+    /**
+     *
+     * @var array
+     */
+    private $array = array();
+
+    /**
+     * 
+     * @param boolean|mysqli_result|mysql_fetch_array $result
+     * @return boolean
+     */
+    function __construct($result) {
+        if (!is_bool($result)) {
+            if (!is_resource($result) && get_class($result) == "mysqli_result") {
+                while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
+                    foreach ($row as $key => $val) {
+                        if (is_int($key)) {
+                            unset($row[$key]);
+                        }
+                    }
+                    $this->array[] = $row;
+                }
+            } else {
+                while ($row = mysql_fetch_array($result)) {
+                    foreach ($row as $key => $val) {
+                        if (is_int($key)) {
+                            unset($row[$key]);
+                        }
+                    }
+                    $this->array[] = $row;
+                }
+            }
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * returns the array of results.
+     * 
+     * @return array
+     */
+    public function getArray() {
+        return $this->array;
+    }
+
+    /**
+     * returns amount of rows.
+     * 
+     * @return integer
+     */
+    function num_rows() {
+        return count($this->array);
+    }
+
+    /**
+     * returns amount of fields of result.
+     * 
+     * @return integer
+     */
+    function num_keys() {
+        return count(array_keys($this->array));
+    }
+
 }
+
 ?>
--- system/helpers/session.php
+++ system/helpers/session.php
@@ -1,28 +1,53 @@
 <?php
+
+/**
+ * This is the helper that will help you with the communication,
+ * save and removal for the session cookies.
+ * 
+ * This helper is tested.
+ */
 class Session {
-	// This is the helper that will help you with the communication, save and removal for the session cookies.
-	// This helper is tested.
-	function __construct()
-	{
-		if (!isset($_SESSION)) session_start();
-	}
-	
-	function getUser($key)
-	{
-		if(isset($_SESSION['user_'.$key])) {
-			return $_SESSION['user_'.$key];
-		}
-	}
-	
-	function setUser($key, $var)
-	{
-		$_SESSION['user_'.$key] = $var;
-		return $_SESSION['user_'.$key];
-	}
-	
-	function deleteUser($key)
-	{
-		unset($_SESSION['user_'.$key]);
-	}
+
+    /**
+     * just call session_start
+     */
+    function __construct() {
+        if (!isset($_SESSION))
+            session_start();
+    }
+
+    /**
+     * 
+     * @param string $key the key for the session
+     * @return mixed
+     */
+    function getUser($key) {
+        if (isset($_SESSION['user_' . $key])) {
+            return $_SESSION['user_' . $key];
+        }
+    }
+
+    /**
+     * Set a session
+     * 
+     * @param string $key
+     * @param mixed $var
+     * @return mixed
+     */
+    function setUser($key, $var) {
+        $_SESSION['user_' . $key] = $var;
+        return $_SESSION['user_' . $key];
+    }
+
+    /**
+     * Delete a session
+     * 
+     * @param string $key
+     */
+    function deleteUser($key) {
+        unset($_SESSION['user_' . $key]);
+    }
+
 }
+
 ?>
