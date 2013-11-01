3ceb00fa585b0c2ff5e3e240309df7db88ceed14
Håkan Nylén <hakan@dun.se>
2013-11-01 12:23:48 +0100

    forgot to correct the query in the test

--- phpunit/helpers/dbTest.php
+++ phpunit/helpers/dbTest.php
@@ -22,7 +22,7 @@ class dbTest extends PHPUnit_Framework_TestCase {
 	function testCreateTable()
 	{
 		$tbl_name = "test";
-		$return = $this->db->create_table($tbl_name, array("id" => "int(10) NOT NULL AUTO INCREMENT", "key" => "varchar(100) NOT NULL"), "id");
+		$return = $this->db->create_table($tbl_name, array("id" => "int(10) NOT NULL AUTO_INCREMENT", "key" => "varchar(100) NOT NULL"), "id");
 		
 		$this->assertTrue($return);
 		$this->db->query("DROP TABLE IF EXISTS ".$tbl_name." CASCADE;");
