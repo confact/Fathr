70de445e5f50907c32f5514444f1f3821347e555
Håkan Nylén <hakan@dun.se>
2013-11-01 12:13:42 +0100

    fixed parse error in test

--- phpunit/helpers/dbTest.php
+++ phpunit/helpers/dbTest.php
@@ -25,7 +25,7 @@ class dbTest extends PHPUnit_Framework_TestCase {
 		$return = $this->db->create_table($tbl_name, array("id" => "int(10) NOT NULL AUTO INCREMENT", "key" => "varchar(100) NOT NULL"), "id");
 		
 		$this->assertTrue($return);
-		$this->db->query("DROP TABLE IF EXISTS ".$tbl_name." CASCADE;")
+		$this->db->query("DROP TABLE IF EXISTS ".$tbl_name." CASCADE;");
 	}
 	
 
