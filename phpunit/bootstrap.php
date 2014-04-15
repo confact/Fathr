483c84eb394203ab8945add61b43660d30718c36
Håkan Nylén <hakan@dun.se>
2014-04-15 10:12:30 +0200

    bootstrap fix

--- phpunit/bootstrap.php
+++ phpunit/bootstrap.php
@@ -43,6 +43,7 @@ class bootstrap {
     public function autoload($class_name) {
         $filename = strtolower($class_name) . '.php';
         $file = "../" . $this->directory . '/' . $filename;
+        var_dump(__FILE__);
         var_dump($file);
         var_dump(file_exists($file));
 		if(file_exists($file) == false)
